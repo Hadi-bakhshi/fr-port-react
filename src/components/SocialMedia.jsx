f599ae0cce64450f7fe8e31171df5a34ebe61693
hadi <hadibakhshi277@gmail.com>
2022-04-10 17:15:52 +0430

    everything is all set

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,17 +1,39 @@
 import React from "react";
-import { BsTwitter, BsInstagram } from "react-icons/bs";
-import { FaFacebookF } from "react-icons/fa";
+import { BsLinkedin, BsInstagram, BsGithub, BsTelegram } from "react-icons/bs";
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a
+        href="https://www.linkedin.com/in/hadi-bakhshi-aa203221b"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsLinkedin />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a
+        href="https://github.com/Hadi-bakhshi"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsGithub />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="https://t.me/haamim27" target="_blank" rel="noreferrer">
+        <BsTelegram />
+      </a>
+    </div>
+    <div>
+      <a
+        href="https://www.instagram.com/hadi_bakhshi27/"
+        target="_blank"
+        rel="noreferrer"
+      >
+        <BsInstagram />
+      </a>
     </div>
   </div>
 );
